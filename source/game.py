af8231c50f75f5384b335a8a4276bfa7b135e4e6
pyabzz <>
2023-12-25 20:36:21 +0000

    Link neighbouring cells

--- source/game.py
+++ source/game.py
@@ -1,16 +1,35 @@
+from source.global_refs import Direction
 from source.cell import Cell
 
 
 class Game:
-    def __init__(self, rows: int, columns: int):
+    def __init__(self, row_count: int, col_count: int):
+        self.row_count = row_count
+        self.col_count = col_count
         self.__cells__ = []
 
-        for row_index in range(rows):
+        for row_index in range(row_count):
             row = []
-            for col_index in range(columns):
+            for col_index in range(col_count):
                 cell = Cell()
                 row.append(cell)
             self.__cells__.append(row)
-        pass
+        self.__link_neighbours()
 
-        # todo: Link neighbours
+    def __link_neighbours(self):
+        for row_index in range(self.row_count):
+            for col_index in range(self.col_count):
+                this_cell = self.__cells__[row_index][col_index]
+
+                has_up_neighbour = row_index != 0
+                has_left_neighbour = col_index != 0
+
+                if has_up_neighbour:
+                    up_neighbour = self.__cells__[row_index - 1][col_index]
+                    this_cell.set_neighbour(Direction.up, up_neighbour)
+                    up_neighbour.set_neighbour(Direction.down, this_cell)
+
+                if has_left_neighbour:
+                    left_neighbour = self.__cells__[row_index][col_index - 1]
+                    this_cell.set_neighbour(Direction.left, left_neighbour)
+                    left_neighbour.set_neighbour(Direction.right, this_cell)
--- test/test_game.py
+++ test/test_game.py
@@ -1,6 +1,7 @@
 import unittest
-from source.cell import Cell
+from source.global_refs import Direction
 from source.game import Game
+from source.cell import Cell
 
 rows = 7
 cols = 8
@@ -19,3 +20,16 @@ class Cell_(unittest.TestCase):
         for r in range(rows):
             for c in range(cols):
                 self.assertIsInstance(sut.__cells__[r][c], Cell)
+
+    def test_middle_cells_have_all_neighbours(self):
+        for r in range(1, rows - 1):
+            for c in range(1, cols - 1):
+                cell = sut.__cells__[r][c]
+                for dir in Direction:
+                    self.assertIsNotNone(cell.get_neighbour(dir))
+
+    def test_first_row_lack_up_neighbours(self):
+        first_row = sut.__cells__[0]
+        for c in range(cols):
+            cell = first_row[c]
+            self.assertIsNone(cell.get_neighbour(Direction.up))
