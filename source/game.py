3d96d3e4f00204f4e30b7cf8384e471f4bae2c46
pyabzz <>
2023-12-25 22:45:03 +0000

    Add iterate_cells function

--- source/game.py
+++ source/game.py
@@ -4,32 +4,51 @@ from source.cell import Cell
 
 class Game:
     def __init__(self, row_count: int, col_count: int):
-        self.row_count = row_count
-        self.col_count = col_count
-        self.__cells__ = []
+        self._row_count = row_count
+        self._col_count = col_count
+        self._cells = []
 
         for row_index in range(row_count):
             row = []
             for col_index in range(col_count):
                 cell = Cell()
                 row.append(cell)
-            self.__cells__.append(row)
-        self.__link_neighbours()
+            self._cells.append(row)
+        self._link_neighbours()
 
-    def __link_neighbours(self):
-        for row_index in range(self.row_count):
-            for col_index in range(self.col_count):
-                this_cell = self.__cells__[row_index][col_index]
+    def _link_neighbours(self):
+        for row_index in range(self._row_count):
+            for col_index in range(self._col_count):
+                this_cell = self._cells[row_index][col_index]
 
                 has_up_neighbour = row_index != 0
                 has_left_neighbour = col_index != 0
 
                 if has_up_neighbour:
-                    up_neighbour = self.__cells__[row_index - 1][col_index]
+                    up_neighbour = self._cells[row_index - 1][col_index]
                     this_cell.set_neighbour(Direction.up, up_neighbour)
                     up_neighbour.set_neighbour(Direction.down, this_cell)
 
                 if has_left_neighbour:
-                    left_neighbour = self.__cells__[row_index][col_index - 1]
+                    left_neighbour = self._cells[row_index][col_index - 1]
                     this_cell.set_neighbour(Direction.left, left_neighbour)
                     left_neighbour.set_neighbour(Direction.right, this_cell)
+
+    def iterate_cells(self, include_boundaries: bool, func, initial_value):
+        row_index_lower_bound = 0 if include_boundaries else 1
+        row_index_upper_bound = (
+            self._row_count if include_boundaries else (self._row_count - 1)
+        )
+        col_index_lower_bound = 0 if include_boundaries else 1
+        col_index_upper_bound = (
+            self._col_count if include_boundaries else (self._col_count - 1)
+        )
+
+        accumulator = initial_value
+
+        for row_index in range(row_index_lower_bound, row_index_upper_bound):
+            for col_index in range(col_index_lower_bound, col_index_upper_bound):
+                this_cell = self._cells[row_index][col_index]
+                accumulator = func(accumulator, this_cell, row_index, col_index)
+
+        return accumulator
--- test/test_game.py
+++ test/test_game.py
@@ -3,33 +3,51 @@ from source.global_refs import Direction
 from source.game import Game
 from source.cell import Cell
 
-rows = 7
-cols = 8
-sut = Game(rows, cols)
+row_count = 7
+col_count = 8
+sut = Game(row_count, col_count)
 
 
 class Cell_(unittest.TestCase):
     def test_init_creates_correct_number_of_rows(self):
-        self.assertEqual(rows, len(sut.__cells__))
+        self.assertEqual(row_count, len(sut._cells))
 
     def test_init_creates_correct_number_of_columns(self):
-        for r in range(rows):
-            self.assertEqual(cols, len(sut.__cells__[r]))
+        for r in range(row_count):
+            self.assertEqual(col_count, len(sut._cells[r]))
 
     def test_init_creates_cell_instances(self):
-        for r in range(rows):
-            for c in range(cols):
-                self.assertIsInstance(sut.__cells__[r][c], Cell)
+        for r in range(row_count):
+            for c in range(col_count):
+                self.assertIsInstance(sut._cells[r][c], Cell)
 
     def test_middle_cells_have_all_neighbours(self):
-        for r in range(1, rows - 1):
-            for c in range(1, cols - 1):
-                cell = sut.__cells__[r][c]
+        for r in range(1, row_count - 1):
+            for c in range(1, col_count - 1):
+                cell = sut._cells[r][c]
                 for dir in Direction:
                     self.assertIsNotNone(cell.get_neighbour(dir))
 
     def test_first_row_lack_up_neighbours(self):
-        first_row = sut.__cells__[0]
-        for c in range(cols):
+        first_row = sut._cells[0]
+        for c in range(col_count):
             cell = first_row[c]
             self.assertIsNone(cell.get_neighbour(Direction.up))
+
+    def test_iterate_cells_hits_all_cells(self):
+        all_cells_count = row_count * col_count
+
+        def bazfunc(accumulator, this_cell, *args, **kwargs):
+            return accumulator + 1
+
+        count = sut.iterate_cells(True, bazfunc, 0)
+        self.assertEqual(all_cells_count, count)
+
+    def test_iterate_cells_hits_interior_cells(self):
+        interior_cells_count = (row_count - 2) * (col_count - 2)
+
+        def bazfunc(accumulator, this_cell, *args, **kwargs):
+            return accumulator + 1
+
+        count = sut.iterate_cells(False, bazfunc, 0)
+        self.assertEqual(interior_cells_count, count)
