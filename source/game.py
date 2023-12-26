34bfb3ced7ebbe9f0b7e2be01b89818613e8ed80
pyabzz <>
2023-12-26 09:22:12 +0000

    Reuse iterate_cells

--- source/game.py
+++ source/game.py
@@ -7,34 +7,35 @@ class Game:
         self._row_count = row_count
         self._col_count = col_count
         self._cells = []
+        self._populate()
+        self._link_neighbours()
 
-        for row_index in range(row_count):
+    def _populate(self):
+        for row_index in range(self._row_count):
             row = []
-            for col_index in range(col_count):
+            for col_index in range(self._col_count):
                 cell = Cell()
                 cell.row = row_index
                 cell.col = col_index
                 row.append(cell)
             self._cells.append(row)
-        self._link_neighbours()
 
     def _link_neighbours(self):
-        for row_index in range(self._row_count):
-            for col_index in range(self._col_count):
-                this_cell = self._cells[row_index][col_index]
+        def visit(cell, ri, ci, acc):
+            has_up_neighbour = ri != 0
+            has_left_neighbour = ci != 0
 
-                has_up_neighbour = row_index != 0
-                has_left_neighbour = col_index != 0
+            if has_up_neighbour:
+                up_neighbour = self._cells[ri - 1][ci]
+                cell.set_neighbour(Direction.up, up_neighbour)
+                up_neighbour.set_neighbour(Direction.down, cell)
 
-                if has_up_neighbour:
-                    up_neighbour = self._cells[row_index - 1][col_index]
-                    this_cell.set_neighbour(Direction.up, up_neighbour)
-                    up_neighbour.set_neighbour(Direction.down, this_cell)
+            if has_left_neighbour:
+                left_neighbour = self._cells[ri][ci - 1]
+                cell.set_neighbour(Direction.left, left_neighbour)
+                left_neighbour.set_neighbour(Direction.right, cell)
 
-                if has_left_neighbour:
-                    left_neighbour = self._cells[row_index][col_index - 1]
-                    this_cell.set_neighbour(Direction.left, left_neighbour)
-                    left_neighbour.set_neighbour(Direction.right, this_cell)
+        self.iterate_cells(True, visit)
 
     def iterate_cells(self, include_boundaries: bool, visit_func, initial_value=None):
         row_index_lower_bound = 0 if include_boundaries else 1
