f1dab315677b0b3d6b57deabc21c1193322c8ab6
pyabzz <>
2024-01-03 09:00:14 +0000

    Add more events

--- source/worm.py
+++ source/worm.py
@@ -7,13 +7,23 @@ from collections import deque
 class Worm:
     _cells: deque[Cell]
     _direction: Direction
+    _step_event: Event
+    _ate_event: Event
     _death_event: Event
 
-    def __init__(self, only_cell: Cell, death_event=None) -> None:
+    def __init__(
+        self,
+        only_cell: Cell,
+        step_event: Event = None,
+        ate_event: Event = None,
+        death_event: Event = None,
+    ) -> None:
         self._cells = deque()
         self._cells.append(only_cell)
         only_cell.be_worm()
         self._direction = Direction.up
+        self._step_event = step_event
+        self._ate_event = ate_event
         self._death_event = death_event
 
     def get_head(self):
@@ -31,16 +41,15 @@ class Worm:
         should_stay_same_length = destination.is_food() == False
         should_die = destination.is_wall() or destination.is_worm()
         if should_die:
-            self._die()
-            return
+            if self._death_event is not None:
+                self._death_event.emit()
+                return
         if should_stay_same_length:
             previous_tail = self._cells.popleft()
             previous_tail.be_blank()
         destination.be_worm()
         self._cells.append(destination)
-
-    def _die(self):
-        if self._death_event is None:
-            pass
-        else:
-            self._death_event.emit()
+        if self._step_event is not None:
+            self._step_event.emit()
+        if self._ate_event is not None:
+            self._ate_event.emit()
--- test/test_worm.py
+++ test/test_worm.py
@@ -7,71 +7,118 @@ from source.worm import Worm
 
 
 class Worm_(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(Worm_, self).__init__(*args, **kwargs)
+        self.step_event = Event()
+        self.step_callback = MagicMock()
+        self.step_event.subscribe(self.step_callback)
+
+        self.ate_event = Event()
+        self.ate_callback = MagicMock()
+        self.ate_event.subscribe(self.ate_callback)
+
+        self.death_event = Event()
+        self.death_callback = MagicMock()
+        self.death_event.subscribe(self.death_callback)
+
+    # ===============================  init  ===============================
+
     def test_init_sets_one_cell_only(self):
         cell = Cell(None, None)
         sut = Worm(cell)
         self.assertEqual(1, sut.get_length())
 
+    # ===============================  step-any  ===============================
+
+    def test_step_when_into_any_cell_emits_step_event(self):
+        for cellType in CellType:
+            destination = Cell(None, None, cellType)
+            initial_head = Cell(None, None)
+            initial_head.get_neighbour = lambda whatever: destination
+            sut = Worm(initial_head, self.step_event, self.ate_event, self.death_event)
+            sut.step()
+            self.step_callback.assert_called()
+
+    # ===============================  step-blank  ===============================
+
     def test_step_moves_head(self):
         destination = Cell(None, None)
         initial_head = Cell(None, None)
         initial_head.get_neighbour = lambda whatever: destination
-        worm = Worm(initial_head)
-        worm.step()
-        self.assertEqual(destination, worm.get_head())
+        sut = Worm(initial_head, self.step_event, self.ate_event, self.death_event)
+        sut.step()
+        self.assertEqual(destination, sut.get_head())
 
-    def test_step_makes_new_cell_worm(self):
+    def test_step_when_into_blank_makes_new_cell_worm(self):
         destination = Cell(None, None)
         initial_head = Cell(None, None)
         initial_head.get_neighbour = lambda whatever: destination
-        worm = Worm(initial_head)
-        worm.step()
+        sut = Worm(initial_head, self.step_event, self.ate_event, self.death_event)
+        sut.step()
         self.assertTrue(destination.is_worm)
 
     def test_step_when_into_blank_makes_tail_cell_blank(self):
         destination = Cell(None, None)
         initial_head = Cell(None, None)
         initial_head.get_neighbour = lambda whatever: destination
-        worm = Worm(initial_head)
-        initial_tail = worm.get_tail()
-        worm.step()
+        sut = Worm(initial_head, self.step_event, self.ate_event, self.death_event)
+        initial_tail = sut.get_tail()
+        sut.step()
         self.assertTrue(initial_tail.is_blank())
 
     def test_step_when_into_blank_keeps_length_same(self):
         destination = Cell(None, None)
         initial_head = Cell(None, None)
         initial_head.get_neighbour = lambda whatever: destination
-        worm = Worm(initial_head)
-        initial_length = worm.get_length()
-        worm.step()
-        self.assertEqual(initial_length, worm.get_length())
+        sut = Worm(initial_head, self.step_event, self.ate_event, self.death_event)
+        initial_length = sut.get_length()
+        sut.step()
+        self.assertEqual(initial_length, sut.get_length())
+
+    # ===============================  step-food  ===============================
 
     def test_step_when_into_food_increments_length(self):
         destination = Cell(None, None, CellType.food)
         initial_head = Cell(None, None)
         initial_head.get_neighbour = lambda whatever: destination
-        worm = Worm(initial_head)
-        initial_length = worm.get_length()
-        worm.step()
-        self.assertEqual(initial_length + 1, worm.get_length())
+        sut = Worm(initial_head, self.step_event, self.ate_event, self.death_event)
+        initial_length = sut.get_length()
+        sut.step()
+        self.assertEqual(initial_length + 1, sut.get_length())
 
-    def test_step_when_into_wall_emets_death_event(self):
+    def test_step_when_into_food_emits_ate_event(self):
+        destination = Cell(None, None, CellType.food)
+        initial_head = Cell(None, None)
+        initial_head.get_neighbour = lambda whatever: destination
+        sut = Worm(initial_head, self.step_event, self.ate_event, self.death_event)
+        sut.step()
+        self.step_callback.assert_called()
+        self.ate_callback.assert_called()
+
+    def test_step_when_into_food_does_not_emit_death_event(self):
+        destination = Cell(None, None, CellType.food)
+        initial_head = Cell(None, None)
+        initial_head.get_neighbour = lambda whatever: destination
+        sut = Worm(initial_head, self.step_event, self.ate_event, self.death_event)
+        sut.step()
+        self.death_callback.assert_not_called()
+
+    # ===============================  step-wall  ===============================
+
+    def test_step_when_into_wall_emit_death_event(self):
         destination = Cell(None, None, CellType.wall)
         initial_head = Cell(None, None)
         initial_head.get_neighbour = lambda whatever: destination
-        death_event = Event()
-        worm = Worm(initial_head, death_event)
-        death_callback = MagicMock()
-        death_event.subscribe(death_callback)
-        worm.step()
-        death_callback.assert_called()
+        sut = Worm(initial_head, self.step_event, self.ate_event, self.death_event)
+        sut.step()
+        self.death_callback.assert_called()
 
     def test_step_when_into_wall_without_death_event_does_nothing(self):
         destination = Cell(None, None, CellType.wall)
         initial_head = Cell(None, None)
         initial_head.get_neighbour = lambda whatever: destination
-        worm = Worm(initial_head)
+        sut = Worm(initial_head, self.step_event, self.ate_event, self.death_event)
         try:
-            worm.step()
+            sut.step()
         except Exception:
             self.fail("Worm death event threw an error")
