9e42211c66df527eda1243e35e3d655a128b1f33
pyabzz <>
2024-01-05 16:56:47 +0000

    .

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 from source.drawer import Drawer
 from source.game import Game
 
-game = Game(18)
+game = Game(24)
 worm = game.add_worm()
 game._drop_food()
 drawer = Drawer()
@@ -13,7 +13,7 @@ def update():
 
 
 game.event_hub.stepped.subscribe(update)
-task = worm.run()
+task = worm.run(0.1, 7)
 res = asyncio.get_event_loop().run_until_complete(task)
 
 drawer.getMouse()
