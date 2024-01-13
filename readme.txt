a3e89f01c9d512798986e66e97218cedcf9f9f53
pyabzz <>
2024-01-13 10:19:45 +0000

    Add yaml file for config

--- readme.txt
+++ readme.txt
@@ -1,2 +1,3 @@
 Python 3.11.0
 tensorflow 2.15.0    pip install tensorflow
+PyYaml-6.0.1         pip install PyYaml
--- src/config.py
+++ src/config.py
@@ -1,20 +1,5 @@
-from configparser import ConfigParser
+import yaml
 
+config = yaml.safe_load(open("config.yaml"))
 
-class Config:
-    filePath: str
-
-    def __init__(self, filePath):
-        self.filePath = filePath
-        self.parser = ConfigParser()
-        self.parser.read(filePath)
-
-    def get_section(self, section):
-        res = {}
-        if self.parser.has_section(section):
-            for param in self.parser.items(section):
-                res[param[0]] = param[1]
-        else:
-            raise Exception(
-                f'Section {section} not found in config file {self.filePath}')
-        return res
+print(config["truck"]["brand"])
