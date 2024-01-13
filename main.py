0353a2a8c236792775f3d747e570c8ed9c587900
pyabzz <>
2024-01-13 21:48:26 +0000

    Use yaml config file

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import random
 import sys
 import asyncio
+from src.config import Config
 from src.game.direction import Direction
 from src.game.drawer import Drawer
 from src.game.game import Game
 
 args = sys.argv
 switch = args[1] if len(args) > 1 else None
-row_count = 40
-col_count = 40
-cell_size = 20
+is_headless = switch == "headless"
+row_count = Config.get("game.row_count")
+col_count = Config.get("game.col_count")
+cell_size = Config.get("game.cell_size")
+food_count = Config.get("game.food_count")
+interval = Config.get("game.interval")
+
+
 game = Game(row_count, col_count)
 snake = game.add_snake()
-game._add_food(80)
+game._add_food(food_count)
 drawer = Drawer(cell_size, row_count, col_count)
 drawer.draw(game.get_cells())
 
@@ -29,14 +35,14 @@ def steer(*args, **kwargs):
 game.event_hub.stepped.subscribe(steer)
 
 
-if switch == "headless":
+if is_headless:
     drawer.getMouse()
     snake.run_sync()
     drawer.draw(game.get_cells())
     drawer.getMouse()
 else:
     game.event_hub.ready_to_draw.subscribe(redraw)
-    task = snake.run_async(0.03)
+    task = snake.run_async(interval)
     res = asyncio.get_event_loop().run_until_complete(task)
 
     drawer.getMouse()
--- src/config.py
+++ src/config.py
@@ -1,5 +1,19 @@
 import yaml
 
-config = yaml.safe_load(open("config.yaml"))
 
-print(config["truck"]["brand"])
+class Config:
+    _instance = None
+
+    @staticmethod
+    def _get():
+        if Config._instance is None:
+            Config._instance = yaml.safe_load(open("config.yaml"))
+        return Config._instance
+
+    @staticmethod
+    def get(path: str):
+        obj = Config._get()
+        segments = path.split(".")
+        for seg in segments:
+            obj = obj[seg]
+        return obj
