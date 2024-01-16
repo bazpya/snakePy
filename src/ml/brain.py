ad50f3ac8e13e14cf615b34aa1ba4e909ee87ae3
pyabzz <>
2024-01-16 17:27:00 +0000

    Add more initialisation params to model

--- src/ml/brain.py
+++ src/ml/brain.py
@@ -1,38 +1,30 @@
-import tensorflow as tf
 import keras
 from keras import layers
+from src.game.global_refs import Anonym
 from src.game.direction import Direction
 
 
 class Brain:
     # _model
 
-    def __init__(
-        self,
-        input_size: int,
-        layer_sizes: list[int],
-        # activation,
-        # kernel_initialiser,
-        # use_bias,
-        # bias_initialiser,
-    ):
+    def __init__(self, model_params: Anonym):
         model_layers = []
         input_layer = layers.Dense(
-            units=layer_sizes[0],
-            # activation=activation,
-            # use_bias=use_bias,
-            # kernel_initializer=kernel_initialiser,
-            # bias_initializer=bias_initialiser,
-            input_shape=(input_size,),
+            units=model_params.layer_sizes[0],
+            activation=model_params.activation,
+            use_bias=model_params.use_bias,
+            kernel_initializer=model_params.kernel_initialiser,
+            bias_initializer=model_params.bias_initialiser,
+            input_shape=(model_params.input_size,),
         )
         model_layers.append(input_layer)
-        for size in layer_sizes[1:]:
+        for size in model_params.layer_sizes[1:]:
             layer = layers.Dense(
                 units=size,
-                # activation=activation,
-                # use_bias=use_bias,
-                # kernel_initializer=kernel_initialiser,
-                # bias_initializer=bias_initialiser,
+                activation=model_params.activation,
+                use_bias=model_params.use_bias,
+                kernel_initializer=model_params.kernel_initialiser,
+                bias_initializer=model_params.bias_initialiser,
             )
             model_layers.append(layer)
         self._model = keras.Sequential(model_layers)
--- src/ml_test/brain_.py
+++ src/ml_test/brain_.py
@@ -1,17 +1,25 @@
 import unittest
+import tensorflow as tf
+from src.game.global_refs import Anonym
 from src.game.direction import Direction
 from src.ml.brain import Brain
 
 
 class Brain_(unittest.TestCase):
-    _sizes = [2, 3, 4, 5]
-    _input_size = 10
+    _model_params = Anonym(
+        input_size=10,
+        layer_sizes=[2, 3, 4, 5],
+        activation=tf.keras.activations.linear,
+        kernel_initialiser=tf.keras.initializers.LecunNormal,
+        use_bias=False,
+        bias_initialiser=tf.keras.initializers.zeros,
+    )
 
     def test_init_makes_correct_layer_sizes(self):
-        sut = Brain(input_size=8, layer_sizes=self._sizes)
+        sut = Brain(self._model_params)
         for i, layer in enumerate(sut._model.layers):
-            self.assertEqual(layer.units, self._sizes[i])
+            self.assertEqual(layer.units, self._model_params.layer_sizes[i])
 
     def test_decide_returns_a_direction(self):
-        sut = Brain(self._input_size, [2, 3, 4, 5])
+        sut = Brain(self._model_params)
         self.assertIsInstance(sut.decide(), Direction)
