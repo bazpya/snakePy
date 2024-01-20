2daf464cbc7128798fe051be1110e895d868adcb
pyabzz <>
2024-01-20 00:07:46 +0000

    Tidy-up

--- src/ml_test/brain_factory_.py
+++
@@ -1,9 +0,0 @@
-# import unittest
-# from src.ml.brain import Brain
-# from src.ml.brain_factory import BrainFactory
-
-
-# class BrainFactory_(unittest.TestCase):
-#     def test_create_returns_brain(self):
-#         sut = BrainFactory()
-#         self.assertIsInstance(sut.create(), Brain)
