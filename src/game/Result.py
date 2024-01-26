2838bfd5058dc427aec9f6e1f323de1e4d76de07
pyabzz <>
2024-01-26 10:56:48 +0000

    Add attributes to result classes

--- src/game/Result.py
+++ src/game/Result.py
@@ -1,14 +1,29 @@
-class SnakeResult:
-    steps_taken: int
-    length: int
+from src.game.cell import Cell
+from src.game.direction import Turn
 
+
+class SnakeResult:
     def __init__(self, steps_taken: int, length: int) -> None:
         self.steps_taken = steps_taken
         self.length = length
 
 
 class GameResult:
-    pass
+    def __init__(
+        self,
+        width: int,
+        height: int,
+        steps_taken: int,
+        length: int,
+        foods: list[Cell],
+        turns: list[Turn],
+    ) -> None:
+        self.width = width
+        self.height = height
+        self.steps_taken = steps_taken
+        self.length = length
+        self.foods = foods
+        self.turns = turns
 
 
 class PlayerResult:
