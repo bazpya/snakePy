4c91add4812ee4cf948399b30fa6359768c4e2c9
pyabzz <>
2024-01-29 13:31:35 +0000

    Add inheritance to result types

--- src/game/Result.py
+++ src/game/Result.py
@@ -3,7 +3,24 @@ import json
 from src.game.global_refs import CauseOfDeath
 
 
-class SnakeResult:
+class Result:
+    def serialise(self):
+        return json.dumps(
+            self.__dict__,
+            default=self.serialiser_func,
+            indent=2,
+        )
+
+    def serialiser_func(self, x):
+        if isinstance(x, Enum):
+            return x.name
+        elif isinstance(x, Result):
+            return x.__dict__
+        else:
+            return x
+
+
+class SnakeResult(Result):
     def __init__(
         self,
         steps_taken: int,
@@ -15,28 +32,13 @@ class SnakeResult:
         self.cause_of_death = cause_of_death
 
 
-class GameResult:
+class GameResult(SnakeResult):
     def __init__(
         self,
         row_count: int,
         col_count: int,
         snake_res: SnakeResult,
-    ) -> None: # use inheritance
+    ) -> None:
         self.row_count = row_count
         self.col_count = col_count
-        self.steps_taken = snake_res.steps_taken
-        self.length = snake_res.length
-        self.cause_of_death = snake_res.cause_of_death
-
-    def serialise(self):
-        return json.dumps(
-            self.__dict__,
-            default=self.serialiser_func,
-            indent=2,
-        )
-
-    def serialiser_func(self, x):
-        if isinstance(x, Enum):
-            return x.name
-        else:
-            return x
+        self.snake = snake_res
