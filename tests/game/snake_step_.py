f352d3179a528cda893350fd1044b68d1c4133f3
pyabzz <>
2024-02-06 17:48:14 +0000

    Bugfix test case

--- tests/game/snake_step_.py
+++ tests/game/snake_step_.py
@@ -45,7 +45,7 @@ class Snake_step_(Snake_sync_):
         origin = Cell()
         sut = Snake(origin)
         origin.get_neighbour = MagicMock()
-        dir = Direction.any()
+        dir = Direction.up  # Must be perpendicular to the initial left
         sut.direction_enque(dir)
         sut.step()
         origin.get_neighbour.assert_called_once_with(dir)
